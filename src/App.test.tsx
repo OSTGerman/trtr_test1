f0c1ca64ff8ebc2ba6d887a322ba19cbe008e467
Yury Babaev <yurybabaev@gmail.com>
2022-03-27 12:37:19 +0300

    Fix: нерабочий тест App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders company filter', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const companyFilterLabelElement = screen.getByText(/КОМПАНИЯ/i);
+  expect(companyFilterLabelElement).toBeInTheDocument();
 });
