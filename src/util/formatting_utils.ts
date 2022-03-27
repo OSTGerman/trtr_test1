11e537e760e74340450552c3e441f0ef744d949f
Yury Babaev <yurybabaev@gmail.com>
2022-03-27 12:44:50 +0300

    Fix: убран ненужный ненужный форматтер даты, используется toLocaleTimeString()

--- src/util/formatting_utils.ts
+++ src/util/formatting_utils.ts
@@ -4,11 +4,16 @@ function padZero(num: number, targetLength: number) {
     return String(num).padStart(targetLength, '0')
 }
 
+const options: Intl.DateTimeFormatOptions = {    
+    hour: '2-digit',
+    minute: '2-digit',
+    hourCycle: 'h23'
+  };
+
 export function getSegmentStartStopStr(segment: Segment): string {
     const startDate = new Date(segment.date);
     const endDate = new Date(startDate.getTime() + segment.duration * 60000);
-    return `${padZero(startDate.getHours(), 2)}:${padZero(startDate.getMinutes(), 2)} –
-            ${padZero(endDate.getHours(), 2)}:${padZero(endDate.getMinutes(), 2)}`;
+    return `${startDate.toLocaleTimeString('ru-RU', options)} – ${endDate.toLocaleTimeString('ru-RU', options)}`    
 }
 
 export function getDurationStr(segment: Segment): string {    
