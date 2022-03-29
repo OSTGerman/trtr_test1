9825493d11dc6719f545cf250b033be6e9fa3578
yura <yura@fvs.ru>
2022-03-29 13:10:03 +0300

    Enhancement: индикатор загрузки данных

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Logo } from './components/logo/Logo'
 import './App.scss'
 import { TransferFilter } from './components/transfer-filter/TransferFilter';
@@ -15,20 +15,20 @@ function App() {
 
   const [tickets, setTickets] = useState<TicketModel[]>([]);
   const [companies, setCompanies] = useState<Company[]>([]);
-  const [companyId, setCompanyId] = useState<string|null>(null);
+  const [companyId, setCompanyId] = useState<string | null>(null);
   const [selectedTransfers, setSelectedTransfers] = useState([true, true, false, false]);
   const [quality, setQuality] = useState<QualityFilterValues>('CHEAPEST');
   const [dataStore] = useState<DataStore>(new DataStore(new AviasalesTestDataProvider()));
   const [updateCount, setUpdateCount] = useState(0);
-  
-  useEffect(() => {             
-      dataStore.fetchData(() => {        
-        setCompanies(dataStore.GetCompanies());
-        setUpdateCount(updateCount => updateCount + 1);
-      });  
+
+  useEffect(() => {
+    dataStore.fetchData(() => {
+      setCompanies(dataStore.GetCompanies());
+      setUpdateCount(updateCount => updateCount + 1);
+    });
   }, [dataStore]);
-  
-  useEffect(() => {    
+
+  useEffect(() => {
     setTickets(dataStore.GetFilteredData(selectedTransfers, companyId, quality));
   }, [dataStore, companyId, companies, selectedTransfers, quality, updateCount]);
 
@@ -39,12 +39,17 @@ function App() {
       </div>
       <div className='app-container'>
         <div className='filters-left-container'>
-          <TransferFilter possibleTransfers={selectedTransfers} onTransfersChanged={transfers => { setSelectedTransfers([...transfers]);}}></TransferFilter>          
-          <CompanyFilter companies={companies} onCompanySelected={companyId => {setCompanyId(companyId);}}></CompanyFilter>
+          <TransferFilter possibleTransfers={selectedTransfers} onTransfersChanged={transfers => { setSelectedTransfers([...transfers]); }}></TransferFilter>
+          <CompanyFilter companies={companies} onCompanySelected={companyId => { setCompanyId(companyId); }}></CompanyFilter>
         </div>
         <div className='main-container'>
-          <QualityFilter initialQuality='CHEAPEST' onFilterChanged={(quality) => {setQuality(quality);}}></QualityFilter>
-          <TicketsRibbon tickets={tickets}></TicketsRibbon>
+          <QualityFilter initialQuality='CHEAPEST' onFilterChanged={(quality) => { setQuality(quality); }}></QualityFilter>
+          {
+            updateCount ?
+              <TicketsRibbon tickets={tickets}></TicketsRibbon>
+              :
+              <div>Загрузка данных...</div>
+          }
         </div>
       </div>
     </div>
